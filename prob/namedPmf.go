84b0c890c3d4f58b439793a69cf1a8258a2fb4e1
dkaslovsky <dkaslovsky@gmail.com>
2020-01-21 21:43:22 -0700

    split into named and numeric pmfs

---
+++ prob/namedPmf.go
@@ -0,0 +1,113 @@
+package prob
+
+import (
+	"fmt"
+)
+
+// NamedPmfElement is a discrete element in a NamedPmf
+type NamedPmfElement struct {
+	Name string
+	Prob float64
+}
+
+// NewNamedPmfElement creates a new NamedPmfElement
+func NewNamedPmfElement(name string, prob float64) *NamedPmfElement {
+	return &NamedPmfElement{
+		Name: name,
+		Prob: prob,
+	}
+}
+
+// NamedPmf is a probability mass function
+type NamedPmf struct {
+	prob map[string]float64
+	sum  float64
+}
+
+// NewNamedPmf creates a new Pmf
+func NewNamedPmf() *NamedPmf {
+	return &NamedPmf{
+		prob: make(map[string]float64),
+	}
+}
+
+// Set sets the value of an element
+func (p *NamedPmf) Set(elem *NamedPmfElement) {
+	p.prob[elem.Name] = elem.Prob
+	p.sum += elem.Prob
+}
+
+// Normalize normalizes the values of the Pmf to sum to 1
+func (p *NamedPmf) Normalize() {
+	if p.sum == 0 {
+		for elem := range p.prob {
+			p.prob[elem] = 0
+		}
+		return
+	}
+
+	for elem := range p.prob {
+		p.prob[elem] /= p.sum
+	}
+	p.sum = 1.0
+}
+
+// Mult multiplies the probability associated with an element by the specified value
+func (p *NamedPmf) Mult(elem string, multVal float64) {
+	curVal, ok := p.prob[elem]
+	if !ok {
+		// TODO: log a warning, print for now
+		fmt.Printf("Attempting to modify nonexisting element [%s]\n", elem)
+		return
+	}
+	p.prob[elem] *= multVal
+	p.sum += curVal * (multVal - 1) // maintain sum by subtracting curVal and adding curVal*multVal
+}
+
+// Prob returns the probability associated with an element
+func (p *NamedPmf) Prob(elem string) float64 {
+	val, ok := p.prob[elem]
+	if !ok {
+		return 0
+	}
+	return val
+}
+
+// Print prints the Pmf
+func (p *NamedPmf) Print() {
+	border := "----------"
+	fmt.Println(border)
+	for elem, prob := range p.prob {
+		fmt.Printf("%s: %0.2f\n", elem, prob)
+	}
+	fmt.Println(border)
+	fmt.Println()
+}
+
+type likelihoodGetterByName interface {
+	GetLikelihood(string) float64
+}
+
+// NamedSuite is a suite of hypotheses with associated probabilities (a Pmf)
+type NamedSuite struct {
+	*NamedPmf
+}
+
+// NewNamedSuite creates a new Suite
+func NewNamedSuite(hypos ...*NamedPmfElement) *NamedSuite {
+	s := &NamedSuite{NewNamedPmf()}
+	for _, hypo := range hypos {
+		s.Set(hypo)
+	}
+	s.Normalize()
+	return s
+}
+
+// Update updates the probabilities based on an observation
+func (s *NamedSuite) Update(obs likelihoodGetterByName) {
+	for hypoName := range s.prob {
+		like := obs.GetLikelihood(hypoName)
+		s.Mult(hypoName, like)
+	}
+	s.Normalize()
+}
---
+++ prob/numericPmf.go
@@ -0,0 +1,113 @@
+package prob
+
+import (
+	"fmt"
+)
+
+// NumericPmfElement is a discrete element in a NumericPmf
+type NumericPmfElement struct {
+	Val  float64
+	Prob float64
+}
+
+// NewNumericPmfElement creates a new NumericPmfElement
+func NewNumericPmfElement(val float64, prob float64) *NumericPmfElement {
+	return &NumericPmfElement{
+		Val:  val,
+		Prob: prob,
+	}
+}
+
+// NumericPmf is a probability mass function
+type NumericPmf struct {
+	prob map[float64]float64
+	sum  float64
+}
+
+// NewNumericPmf creates a new Pmf
+func NewNumericPmf() *NumericPmf {
+	return &NumericPmf{
+		prob: make(map[float64]float64),
+	}
+}
+
+// Set sets the value of an element
+func (p *NumericPmf) Set(elem *NumericPmfElement) {
+	p.prob[elem.Val] = elem.Prob
+	p.sum += elem.Prob
+}
+
+// Normalize normalizes the values of the Pmf to sum to 1
+func (p *NumericPmf) Normalize() {
+	if p.sum == 0 {
+		for elem := range p.prob {
+			p.prob[elem] = 0
+		}
+		return
+	}
+
+	for elem := range p.prob {
+		p.prob[elem] /= p.sum
+	}
+	p.sum = 1.0
+}
+
+// Mult multiplies the probability associated with an element by the specified value
+func (p *NumericPmf) Mult(elem float64, multVal float64) {
+	curVal, ok := p.prob[elem]
+	if !ok {
+		// TODO: log a warning, print for now
+		fmt.Printf("Attempting to modify nonexisting element [%s]\n", elem)
+		return
+	}
+	p.prob[elem] *= multVal
+	p.sum += curVal * (multVal - 1) // maintain sum by subtracting curVal and adding curVal*multVal
+}
+
+// Prob returns the probability associated with an element
+func (p *NumericPmf) Prob(elem float64) float64 {
+	val, ok := p.prob[elem]
+	if !ok {
+		return 0
+	}
+	return val
+}
+
+// Print prints the Pmf
+func (p *NumericPmf) Print() {
+	border := "----------"
+	fmt.Println(border)
+	for elem, prob := range p.prob {
+		fmt.Printf("%v: %0.2f\n", elem, prob)
+	}
+	fmt.Println(border)
+	fmt.Println()
+}
+
+type likelihoodGetterByNumeric interface {
+	GetLikelihood(float64) float64
+}
+
+// NumericSuite is a suite of hypotheses with associated probabilities (a Pmf)
+type NumericSuite struct {
+	*NumericPmf
+}
+
+// NewNumericSuite creates a new Suite
+func NewNumericSuite(hypos ...*NumericPmfElement) *NumericSuite {
+	s := &NumericSuite{NewNumericPmf()}
+	for _, hypo := range hypos {
+		s.Set(hypo)
+	}
+	s.Normalize()
+	return s
+}
+
+// Update updates the probabilities based on an observation
+func (s *NumericSuite) Update(obs likelihoodGetterByNumeric) {
+	for hypoName := range s.prob {
+		like := obs.GetLikelihood(hypoName)
+		s.Mult(hypoName, like)
+	}
+	s.Normalize()
+}
