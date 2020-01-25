987f7ff63ec242bbd78da6d2f6fc0dc18bc2ae64
dkaslovsky <dkaslovsky@gmail.com>
2020-01-24 21:09:54 -0700

    locomotive power law

--- exercises/locomotive.go
+++ exercises/locomotive.go
@@ -2,6 +2,7 @@ package exercises
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/dkaslovsky/GoThinkBayes/prob"
 )
@@ -11,45 +12,119 @@ import (
 // One day you see a locomotive with the number 60.
 // Estimate how many loco- motives the railroad has.
 
-func uniform(low, high int) (elems []*prob.NumericPmfElement) {
-	for i := low; i <= high; i++ {
+type bound struct {
+	low  int
+	high int
+}
+
+var bounds = []bound{
+	bound{
+		low:  1,
+		high: 500,
+	},
+	bound{
+		low:  1,
+		high: 1000,
+	},
+	bound{
+		low:  1,
+		high: 2000,
+	},
+}
+
+func uniform(b bound) (elems []*prob.NumericPmfElement) {
+	for i := b.low; i <= b.high; i++ {
 		elems = append(elems, prob.NewNumericPmfElement(float64(i), 1))
 	}
 	return elems
 }
 
-// Locomotive runs the locomotive problem
-func Locomotive() {
-	uniformBounds := []struct {
-		low  int
-		high int
-	}{
-		{
-			low:  1,
-			high: 500,
-		},
-		{
-			low:  1,
-			high: 1000,
-		},
-		{
-			low:  1,
-			high: 2000,
-		},
+func powerLaw(b bound, alpha float64) (elems []*prob.NumericPmfElement) {
+	a := -alpha
+	for i := b.low; i <= b.high; i++ {
+		n := float64(i)
+		elems = append(elems, prob.NewNumericPmfElement(n, math.Pow(n, a)))
 	}
+	return elems
+}
 
-	for _, uBound := range uniformBounds {
-		hypos := uniform(uBound.low, uBound.high)
+// locomotive observations (likelihood function) are the same as that of the dice problem
+type locomotiveObservation struct {
+	*diceObservation
+}
+
+func newLocomotiveObservation(val int) *locomotiveObservation {
+	return &locomotiveObservation{
+		&diceObservation{float64(val)},
+	}
+}
+
+func locomototiveUniformSingleObservation() {
+	fmt.Println("Uniform Single Observation")
+
+	for _, bound := range bounds {
+		hypos := uniform(bound)
+		l := prob.NewNumericSuite(hypos...)
+		l.Update(newLocomotiveObservation(60))
+
+		fmt.Printf("Upper bound: %d, Posterior mean: %0.2f\n", bound.high, l.Mean())
+	}
+}
+
+func locomototiveUniformMultipleObservations() {
+	fmt.Println("Uniform Multiple Observation")
+
+	for _, bound := range bounds {
+		hypos := uniform(bound)
 		l := prob.NewNumericSuite(hypos...)
 
-		// observations (likelihood function) are the same as that of the dice problem
 		obs := []prob.NumericSuiteObservation{
-			&diceObservation{60},
-			&diceObservation{30},
-			&diceObservation{90},
+			newLocomotiveObservation(60),
+			newLocomotiveObservation(30),
+			newLocomotiveObservation(90),
 		}
 		l.MultiUpdate(obs)
 
-		fmt.Printf("Upper bound: %d, Posterior mean: %0.2f\n", uBound.high, l.Mean())
+		fmt.Printf("Upper bound: %d, Posterior mean: %0.2f\n", bound.high, l.Mean())
 	}
+}
+
+func locomotivePowerLawSingleObservation() {
+	fmt.Println("Power Law Single Observation")
+
+	alpha := 1.0
+	for _, bound := range bounds {
+		hypos := powerLaw(bound, alpha)
+		l := prob.NewNumericSuite(hypos...)
+		l.Update(newLocomotiveObservation(60))
+
+		fmt.Printf("Upper bound: %d, Posterior mean: %0.2f\n", bound.high, l.Mean())
+	}
+}
+
+func locomotivePowerLawMultipleObservation() {
+	fmt.Println("Power Law Multiple Observation")
+
+	alpha := 1.0
+	for _, bound := range bounds {
+		hypos := powerLaw(bound, alpha)
+		l := prob.NewNumericSuite(hypos...)
+
+		obs := []prob.NumericSuiteObservation{
+			newLocomotiveObservation(60),
+			newLocomotiveObservation(30),
+			newLocomotiveObservation(90),
+		}
+		l.MultiUpdate(obs)
+
+		fmt.Printf("Upper bound: %d, Posterior mean: %0.2f\n", bound.high, l.Mean())
+	}
+}
+
+// Locomotive runs the locomotive problem
+func Locomotive() {
+	locomototiveUniformSingleObservation()
+	locomototiveUniformMultipleObservations()
+	locomotivePowerLawSingleObservation()
+	locomotivePowerLawMultipleObservation()
 }
